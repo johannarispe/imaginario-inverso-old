1b52576fa7beea9216b437bdc2e8e5807dd2a8f4
thiago hersan <thiago.hersan@gmail.com>
2014-12-03 23:21:27 +0000

    adiciona simples celery task para ler porta serial

---
+++ celeryTest/tasks.py
@@ -0,0 +1,18 @@
+from time import sleep
+from celery import Celery
+import serial
+
+app = Celery('tasks', broker='redis://localhost')
+port = serial.Serial("/dev/ttyAMA0", baudrate=4800, timeout=1.0)
+
+@app.task
+def add(x, y):
+    return x+y
+
+@app.task
+def readSerial():
+    while True:
+        data = port.readline()
+        if data:
+            print ("\nreceived:\n"+data[:-1])
+            sleep(0.1)
